763806b7d6b2150d28653a008fa0e8a8380bb81f
SumitMaharana-783 <sumitmaharana@beginning.co.in>
2024-12-14 19:14:25 +0530

    CORS policy changed

--- backend/server.js
+++ backend/server.js
@@ -6,20 +6,27 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
+
 dotenv.config();
+
 const app = express();
 const server = http.createServer(app);
+
 const io = socketIo(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
-    methods: ["GET", "POST", "PUT"]
-  }
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+    credentials: true,
+  },
 });
 
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL,
-  credentials: true
+  methods: ["GET", "POST", "PUT"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-auth-token"],
+  credentials: true,
 }));
 app.use(express.json());
 
@@ -30,9 +37,21 @@ app.use('/api/tasks', taskRoutes(io));
 // Socket.IO connection handling
 io.on('connection', (socket) => {
   console.log('New client connected');
+
+  // Event handling can be customized here
+  socket.on('customEvent', (data) => {
+    console.log('Received customEvent:', data);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
 });
 
 const PORT = process.env.PORT || 5000;
